8e120f529d30c7b511e758da36716a6ed8072c4e
Connor Abbott <cwabbott0@gmail.com>
2018-04-18 21:14:57 -0400

    fix indentation

--- semant.ml
+++ semant.ml
@@ -105,37 +105,37 @@ let check functions =
     (* Return a semantically-checked expression, i.e., with a type *)
     let rec expr = function
         Literal  l -> (Int, SLiteral l)
-                    | Fliteral l -> (Float, SFliteral l)
-                    | BoolLit l  -> (Bool, SBoolLit l)
-                    | TupleLit(el) -> 
-                            let el' = List.map expr el in
-                            let types = List.map (fun (t, _) -> t) el' in
-                            (Tuple(types), STupleLit(el'))
-                    | Noexpr     -> (Void, SNoexpr)
-                    | Id s       -> (type_of_identifier s, SId s)
-                    | Assign(var, e) as ex -> 
-                            let lt = type_of_identifier var
+      | Fliteral l -> (Float, SFliteral l)
+      | BoolLit l  -> (Bool, SBoolLit l)
+      | TupleLit(el) -> 
+          let el' = List.map expr el in
+          let types = List.map (fun (t, _) -> t) el' in
+          (Tuple(types), STupleLit(el'))
+      | Noexpr     -> (Void, SNoexpr)
+      | Id s       -> (type_of_identifier s, SId s)
+      | Assign(var, e) as ex -> 
+          let lt = type_of_identifier var
           and (rt, e') = expr e in
-                            let err = "illegal assignment " ^ string_of_typ lt ^ " = " ^ 
+          let err = "illegal assignment " ^ string_of_typ lt ^ " = " ^ 
             string_of_typ rt ^ " in " ^ string_of_expr ex
-                            in (check_assign lt rt err, SAssign(var, (rt, e')))
-                    | Unop(op, e) as ex -> 
-                            let (t, e') = expr e in
-                            let ty = match op with
+          in (check_assign lt rt err, SAssign(var, (rt, e')))
+      | Unop(op, e) as ex -> 
+          let (t, e') = expr e in
+          let ty = match op with
             Neg when t = Int || t = Float -> t
-                    | Not when t = Bool -> Bool
+      | Not when t = Bool -> Bool
           | Not when t = Qubit -> Qubit
           | _ -> raise (Failure ("illegal unary operator " ^ 
                                  string_of_uop op ^ string_of_typ t ^
                                  " in " ^ string_of_expr ex))
-                            in (ty, SUnop(op, (t, e')))
+          in (ty, SUnop(op, (t, e')))
           | Binop(e1, op, e2) as e -> 
-                  let (t1, e1') = expr e1 
+              let (t1, e1') = expr e1 
           and (t2, e2') = expr e2 in
-                  (* All binary operators require operands of the same type *)
-                  let same = t1 = t2 in
-                  (* Determine expression type based on operator and operand types *)
-                  let ty = match op with
+              (* All binary operators require operands of the same type *)
+              let same = t1 = t2 in
+              (* Determine expression type based on operator and operand types *)
+              let ty = match op with
             Add | Sub | Mult | Div when same && t1 = Int   -> Int
           | Add | Sub | Mult | Div when same && t1 = Float -> Float
           | Equal | Neq            when same               -> Bool
@@ -143,59 +143,59 @@ let check functions =
                      when same && (t1 = Int || t1 = Float) -> Bool
           | And | Or when same && t1 = Bool -> Bool
           | _ -> raise (
-              Failure ("illegal binary operator " ^
+            Failure ("illegal binary operator " ^
                        string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
                        string_of_typ t2 ^ " in " ^ string_of_expr e))
-                  in (ty, SBinop((t1, e1'), op, (t2, e2')))
+              in (ty, SBinop((t1, e1'), op, (t2, e2')))
           | Call(fname, args) as call -> 
-                  let fd = find_func fname in
-                  let param_length = List.length fd.formals in
-                  if List.length args != param_length then
-                      raise (Failure ("expecting " ^ string_of_int param_length ^ 
+              let fd = find_func fname in
+              let param_length = List.length fd.formals in
+              if List.length args != param_length then
+                raise (Failure ("expecting " ^ string_of_int param_length ^ 
                             " arguments in " ^ string_of_expr call))
       else let check_call (ft, _) e = 
-          let (et, e') = expr e in 
-          let err = "illegal argument found " ^ string_of_typ et ^
+        let (et, e') = expr e in 
+        let err = "illegal argument found " ^ string_of_typ et ^
               " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e
-          in (check_assign ft et err, e')
-          in 
+        in (check_assign ft et err, e')
+        in 
           let args' = List.map2 check_call fd.formals args
           in (fd.typ, SCall(fname, args'))
           in
 
     let check_bool_expr e = 
-        let (t', e') = expr e
+      let (t', e') = expr e
       and err = "expected Boolean expression in " ^ string_of_expr e
       in if t' != Bool then raise (Failure err) else (t', e') 
-        in
+      in
 
     (* Return a semantically-checked statement i.e. containing sexprs *)
     let rec check_stmt = function
-        Expr e -> SExpr(expr e)
+      Expr e -> SExpr(expr e)
           | If(p, b1, b2) ->
-                  SIf(check_bool_expr p, check_stmt b1, check_stmt b2)
+              SIf(check_bool_expr p, check_stmt b1, check_stmt b2)
           | For(e1, e2, e3, st) ->
-                  SFor(expr e1, check_bool_expr e2, expr e3, check_stmt st)
+              SFor(expr e1, check_bool_expr e2, expr e3, check_stmt st)
           | While(p, s) ->
-                  SWhile(check_bool_expr p, check_stmt s)
+              SWhile(check_bool_expr p, check_stmt s)
           | Return e -> let (t, e') = expr e in
           if t = func.typ then SReturn (t, e') 
           else raise (
-              Failure ("return gives " ^ string_of_typ t ^ " expected " ^
+            Failure ("return gives " ^ string_of_typ t ^ " expected " ^
            string_of_typ func.typ ^ " in " ^ string_of_expr e))
 
           (* A block is correct if each statement is correct and nothing
            follows any Return statement.  Nested blocks are flattened. *)
           | Block sl -> 
-                  let rec check_stmt_list = function
-                      [Return _ as s] -> [check_stmt s]
+              let rec check_stmt_list = function
+                [Return _ as s] -> [check_stmt s]
           | Return _ :: _   -> raise (Failure "nothing may follow a return")
           | Block sl :: ss  -> check_stmt_list (sl @ ss) (* Flatten blocks *)
             | s :: ss         -> check_stmt s :: check_stmt_list ss
             | []              -> []
     in SBlock(check_stmt_list sl)
 
-                  in (* body of check_function *)
+              in (* body of check_function *)
     let sbody = check_stmt (Block func.body)
     in
     { styp = func.typ;
